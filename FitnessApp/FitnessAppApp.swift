667d5f8cffbcdc2fc9d478a97ffe9782839c399f
Linhua Zhou <zhoulinhua0@gmail.com>
2026-05-27 23:19:37 +0800

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by 周琳桦 on 2026/5/27.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
